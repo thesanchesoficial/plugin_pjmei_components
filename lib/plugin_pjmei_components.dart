431683b54b0b121e43ce0432fc4924c3c40170f8
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2023-02-21 17:50:02 -0300

    export de plugins

--- lib/plugin_pjmei_components.dart
+++ lib/plugin_pjmei_components.dart
@@ -3,6 +3,8 @@ export './utils/utils.dart';
 export 'package:go_router/go_router.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:dynamic_color/dynamic_color.dart';
+export 'package:extended_masked_text/extended_masked_text.dart';
+export 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'plugin_pjmei_components_platform_interface.dart';
 
 class PluginPjmeiComponents {
---
+++ lib/utils/functions/responsiveness/screen_size.dart
@@ -0,0 +1,18 @@
+// import 'package:backoffice/main/main.dart';
+// import 'package:flutter/material.dart';
+
+// changeScreenSize(context) {
+//   if (MediaQuery.of(context).size.width >= 600) {
+//     if(MediaQuery.of(context).size.width < 840) {
+//       appStore.setIsFoldable(true);
+//       appStore.setIsWeb(true);
+//     } else {
+//       appStore.setIsWeb(true);
+//     }
+//   } else {
+//     if(appStore.isWeb || appStore.isFoldable) {
+//       appStore.setIsWeb(false);
+//       appStore.setIsFoldable(false);
+//     }
+//   }
+// }
