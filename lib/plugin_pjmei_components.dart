ac53e33353fe11e6d46801132c2ee0122c04b3a8
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2023-02-21 22:29:39 -0300

    mudancas no go_router

--- lib/plugin_pjmei_components.dart
+++ lib/plugin_pjmei_components.dart
@@ -1,6 +1,6 @@
 export './ui/ui.dart';
 export './utils/utils.dart';
-export 'package:go_router/go_router.dart';
+export 'package:go_router_flow/go_router_flow.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:dynamic_color/dynamic_color.dart';
 export 'package:extended_masked_text/extended_masked_text.dart';
@@ -8,7 +8,7 @@ export 'package:eva_icons_flutter/eva_icons_flutter.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:flutter_tags/flutter_tags.dart';
 export 'package:jwt_decoder/jwt_decoder.dart';
-export 'package:go_router/go_router.dart';
+export 'package:url_launcher/url_launcher.dart';
 import 'plugin_pjmei_components_platform_interface.dart';
 
 class PluginPjmeiComponents {
