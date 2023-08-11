c23241bff8e19e9d4c8a7b52ea643026328e4256
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2023-08-11 09:54:54 -0300

    correcao no navigation bar

--- lib/ui/components/mobile_bottom_navigation_bar_widget.dart
+++ lib/ui/components/mobile_bottom_navigation_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:plugin_pjmei_components/plugin_pjmei_components.dart';
 
 class MobileBottomNavigationBar extends StatelessWidget {
@@ -7,21 +8,25 @@ class MobileBottomNavigationBar extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    if(appSM.isWeb) {
-      return const SizedBox();
-    } else {
-      return NavigationBar(
-        selectedIndex: appSM.selectedIndex,
-        onDestinationSelected: (value) {
-          appSM.setSelectedIndex(value);
-        },
-        destinations: screens.map((e) => NavigationDestination(
-          label: e.title,
-          icon: Icon(e.icon),
-          selectedIcon: Icon(e.icon),
-          tooltip: e.title,
-        )).toList(),
-      );
-    }
+    return Observer(
+      builder: (context) {
+        if(appSM.isWeb) {
+          return const SizedBox();
+        } else {
+          return NavigationBar(
+            selectedIndex: appSM.selectedIndex,
+            onDestinationSelected: (value) {
+              appSM.setSelectedIndex(value);
+            },
+            destinations: screens.map((e) => NavigationDestination(
+              label: e.title,
+              icon: Icon(e.icon),
+              selectedIcon: Icon(e.icon),
+              tooltip: e.title,
+            )).toList(),
+          );
+        }
+      }
+    );
   }
 }
