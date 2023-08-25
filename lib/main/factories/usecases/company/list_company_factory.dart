814b63a98e195a41fcacc4261120b91593d31ad1
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2023-08-25 11:31:25 -0300

    nova rota para listar empresas

--- lib/main/factories/usecases/company/list_company_factory.dart
+++ lib/main/factories/usecases/company/list_company_factory.dart
@@ -2,5 +2,5 @@ import 'package:plugin_pjmei_components/plugin_pjmei_components.dart';
 
 ListCompanies makeRemoteGetListCompanies({Map<String, dynamic>? params}) => RemoteListCompanies(
   httpClient: makeHttpAdapter(),
-  url: makeApiUrl('venver/empresa/${userSM.user?.id}', params: params),
+  url: makeApiUrl('venver/company/user/${userSM.user?.id}', params: params),
 );
