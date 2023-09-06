74d37a2b00ed8f3d16d84c05486d5072736d8b90
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2023-09-06 19:33:23 -0300

    ajustes no ccmei e cnpj

--- lib/main/factories/usecases/cnpj/datasource.dart
+++ lib/main/factories/usecases/cnpj/datasource.dart
@@ -2,8 +2,8 @@ part of '../usecases.dart';
 
 class _CnpjApi {
 
-  Future<CnpjEntity> find({required String cnpj}) {
-    return makeRemoteGetCnpj(cnpj).exec();
+  Future<CnpjEntity> find({required String cnpj, bool ignoreAuth = false}) {
+    return makeRemoteGetCnpj(cnpj, ignoreAuth: ignoreAuth).exec();
   }
 
   Future<CnpjEntity> update() {
