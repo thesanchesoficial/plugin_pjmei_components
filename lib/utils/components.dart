49d3a85ee7340c201a254342dc2b8d6fb02d50e4
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2023-09-18 13:54:49 -0300

    add white label online

--- lib/utils/components.dart
+++ lib/utils/components.dart
@@ -33,12 +33,94 @@ class Main {
     } catch (e) {
       log('error initializing state management');
     }
+
     WhiteLabelEntity.current = whiteLabel;
+    appSM.setWhiteLabel(whiteLabel);
+    
     if(environment == EnvironmentType.development) {
       Environment.current = Development();
     } else {
       Environment.current = Production();
     }
+
+    minimalVersion = minimalVersionApp;
+    logoutRedirect = logoutPathRedirect;
+    userProfile = pathProfileUser;
+    
+    try {
+      if (isFirstLoading) {
+        usePathUrlStrategy();
+        isFirstLoading = false;
+      }
+    } catch (e) {
+      log('error removing hastag from url');
+    }
+    try {
+      if(preload != null) {
+        await preload();
+      }
+    } catch (e) {
+      log('error starting preload');
+    }
+    try {
+      if(listenUser != null) {
+        checkUserNotifier.addListener(listenUser);
+      }
+    } catch (e) {
+      log('error listening for user changes');
+    }
+    try {
+      if(firebaseOptions != null) {
+        await Firebase.initializeApp(options: firebaseOptions);
+      }
+    } catch (e) {
+      log('error initializing firebase');
+    }
+    try {
+      runApp(home);
+    } catch (e) {
+      log('error initializing system');
+    }
+  }
+
+  static Future online({
+    required String whiteLabel,
+    required EnvironmentType environment,
+    required Widget home,
+    Function()? preload,
+    void Function()? listenUser,
+    required int minimalVersionApp,
+    FirebaseOptions? firebaseOptions,
+    required String logoutPathRedirect,
+    required String pathProfileUser,
+  }) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    try {
+      final GetIt getIt = GetIt.I;
+      getIt.registerSingleton<AppSMStore>(AppSMStore());
+      getIt.registerSingleton<CartSMStore>(CartSMStore());
+      getIt.registerSingleton<CompanySMStore>(CompanySMStore());
+      getIt.registerSingleton<EcommerceSMStore>(EcommerceSMStore());
+      getIt.registerSingleton<ModuleSMStore>(ModuleSMStore());
+      getIt.registerSingleton<UserSMStore>(UserSMStore());
+    } catch (e) {
+      log('error initializing state management');
+    }
+
+    try {
+      WhiteLabelEntity temp = await Api.whiteLabel.find(id: whiteLabel);
+      WhiteLabelEntity.current = temp;
+      appSM.setWhiteLabel(temp);
+    } catch (e) {
+      log('error initializing white label');
+    }
+    
+    if(environment == EnvironmentType.development) {
+      Environment.current = Development();
+    } else {
+      Environment.current = Production();
+    }
+
     minimalVersion = minimalVersionApp;
     logoutRedirect = logoutPathRedirect;
     userProfile = pathProfileUser;
