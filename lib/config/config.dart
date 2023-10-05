ca6dbc7cf65a41cf31491ad31e93b37083a80eea
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2023-10-05 16:15:40 -0300

    cachoeiro prod

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,6 +2,7 @@ export './framework/application.dart';
 export './framework/environment.dart';
 export './environment/development.dart';
 export './environment/production.dart';
+export 'white_label/cachoeiro_prod.dart';
 export 'white_label/pjmei_dev.dart';
 export 'white_label/pjmei_prod.dart';
 export 'white_label/serra_prod.dart';
