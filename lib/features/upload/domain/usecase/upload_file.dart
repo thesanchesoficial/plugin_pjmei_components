d797908499f9efc51d8363182c4e6852aaa47b30
Matheus Sanches <sanchesmiranda.matheus@gmail.com>
2024-03-08 10:59:03 -0300

    upload file fix

--- lib/features/upload/domain/usecase/upload_file.dart
+++ lib/features/upload/domain/usecase/upload_file.dart
@@ -1,5 +1,6 @@
 import 'package:plugin_pjmei_components/features/upload/domain/entity/file_entity.dart';
+import 'package:http_parser/http_parser.dart';
 
 abstract class UploadFile {
-  Future<FileEntity> exec(List<int>? file, {bool log = false});
+  Future<FileEntity> exec(List<int>? file, {String? filename, MediaType? contentType, bool isPublic = false, bool log = false});
 }
